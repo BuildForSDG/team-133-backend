3ff0272c1752279b74ef5960d6dc898fa6251aa2
purplesmile3 <puritychepkurui5@gmail.com>
2020-05-17 11:51:12 +0300

    app file

--- src/app.js
+++ src/app.js
@@ -1,11 +1,30 @@
-const express = require('express');
-const bodyParser     =        require("body-parser");
-const db = require('./config/database').db;
-const productRouter = require('../src/routes/product.route')
+const express= require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const cors= require('cors');
 
-let app = express();
-app.use(bodyParser.json());
-  
-app.use(productRouter);
+const postRoute= require('./routes/postRoute');
 
-module.exports = app
+const app= express();
+
+
+
+
+//connecting to mongoose cluster
+mongoose.connect('mongodb+srv://puritys:admin@cluster0-tyaxn.mongodb.net/test?retryWrites=true&w=majority',
+{ useNewUrlParser: true,
+  useUnifiedTopology: true })
+.then(() => {
+    console.log('Successfully connected to MongoDB Atlas!');
+  })
+  .catch((error) => {
+    console.log('Unable to connect to MongoDB Atlas!');
+    console.error(error);
+  });
+
+  app.use(cors());
+  app.use(bodyParser.json());
+ app.use('/api/posts',postRoute);
+
+
+  module.exports= app;
