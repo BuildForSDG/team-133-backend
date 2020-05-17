391116cdd66a52ae2d94888f336572195ac94658
purplesmile3 <puritychepkurui5@gmail.com>
2020-05-17 22:29:13 +0300

    comments schema

--- src/models/comments.js
+++ src/models/comments.js
@@ -1,13 +1,15 @@
+/*eslint-disable*/
 const mongoose = require('mongoose');
 
 const commentSchema = mongoose.Schema({
   text: { type: String, required: true },
   post: {
  type: mongoose.Schema.Types.ObjectId, ref:"Post", required:true
-  },
-  dateCreated: { type:Date, required: true}
+  }
 
   
+},{
+  timestamps:true
 });
 
 module.exports = mongoose.model('Comment', commentSchema);
